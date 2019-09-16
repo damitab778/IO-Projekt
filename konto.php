72dc3ceed9f535eec96f38ec059738f9b5636b77
Damian Tabaka <informatyka.tabaka1ti@gmail.com>
2019-09-16 15:20:40 +0200

    Update konto.php

--- konto.php
+++ konto.php
@@ -8,7 +8,7 @@
 					////////////////////////////////////
 						     ####Edycja dzieci####
 					////////////////////////////////////
-					
+								$iloscbach = $_SESSION['iloscbach']+1;
 	  if((isset($_POST['kwota0']))AND(!isset($_POST['dziecko_0']))
 	AND(!isset($_POST['dziecko_1']))AND(!isset($_POST['dziecko_2']))
 	AND(!isset($_POST['dziecko_3']))AND(!isset($_POST['dziecko_4']))
